9869dd265307ade9505062122312122045feb54d
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-01-27 14:16:03 +0100

    Update gimp-git-junest.sh - add libdebuginfod.so

--- gimp-git-junest.sh
+++ gimp-git-junest.sh
@@ -2,7 +2,7 @@
 
 APP=gimp-git
 BIN="gimp" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
-DEPENDENCES="dbus gdb ghostscript gjs highway libgudev libunwind luajit nspr python python-gobject qoi-headers-git" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
+DEPENDENCES="dbus gdb ghostscript gjs highway libelf libgudev libunwind luajit nspr python python-gobject qoi-headers-git" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
 BASICSTUFF="binutils debugedit gzip"
 #COMPILERS="base-devel"
 
@@ -15,7 +15,7 @@ SHARESAVED="xml"
 #lib_audio_keywords="alsa jack pipewire pulse"
 lib_browser_launcher="gio-launch-desktop libasound.so libatk-bridge libatspi libcloudproviders libdb- libdl.so libedit libepoxy libgtk-3.so.0 libjson-glib libnssutil libpthread.so librt.so libtinysparql libwayland-cursor libX11-xcb.so libxapp-gtk3-module.so libXcursor libXdamage libXi.so libxkbfile.so libXrandr p11 pk"
 LIBSAVED="gdk-pixbuf libjson libsoxr.so libuuid.so libsodium.so pixmap \
-libpython libavcodec.so libgudev libpoppler.so libhogweed.so libgjs.so girepository libgpgme.so libhwy.so libassuan.so $lib_audio_keywords $lib_browser_launcher"
+libpython libavcodec.so libgudev libpoppler.so libhogweed.so libgjs.so girepository libgpgme.so libhwy.so libassuan.so libdebuginfod.so $lib_audio_keywords $lib_browser_launcher"
 
 [ -n "$lib_browser_launcher" ] && DEPENDENCES="$DEPENDENCES xapp hicolor-icon-theme"
 
