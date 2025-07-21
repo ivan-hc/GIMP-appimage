5f5006bfc70c1753c85efd260c3a45a3d8ebd271
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-07-21 19:17:37 +0200

    Update gimp-junest.sh - add mypaint
    fix https://github.com/ivan-hc/GIMP-appimage/issues/45

--- gimp-junest.sh
+++ gimp-junest.sh
@@ -2,7 +2,7 @@
 
 APP=gimp
 BIN="$APP" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
-DEPENDENCES="adwaita-icon-theme ffmpeg graphviz python python-cairo python-gobject sdl2" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
+DEPENDENCES="adwaita-icon-theme ffmpeg graphviz mypaint mypaint-brushes python python-cairo python-gobject sdl2" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
 #BASICSTUFF="binutils debugedit gzip"
 #COMPILERS="base-devel"
 
@@ -10,11 +10,11 @@ DEPENDENCES="adwaita-icon-theme ffmpeg graphviz python python-cairo python-gobje
 #	KEYWORDS TO FIND AND SAVE WHEN COMPILING THE APPIMAGE
 #############################################################################
 
-BINSAVED="fc- gdb"
-SHARESAVED="xml"
+BINSAVED="fc- gdb mypaint"
+SHARESAVED="xml mypaint"
 #lib_audio_keywords="alsa jack pipewire pulse"
 lib_browser_launcher="gio-launch-desktop libasound.so libatk-bridge libatspi libcloudproviders libdb- libdl.so libedit libepoxy libgtk-3.so.0 libjson-glib libnssutil libpthread.so librt.so libtinysparql libwayland-cursor libX11-xcb.so libxapp-gtk3-module.so libXcursor libXdamage libXi.so libxkbfile.so libXrandr p11 pk"
-LIBSAVED="gdk-pixbuf libjson libsoxr.so libuuid.so libSDL libsodium.so pixmap tinysparql girepository $lib_audio_keywords $lib_browser_launcher"
+LIBSAVED="gdk-pixbuf libjson libsoxr.so libuuid.so libSDL libsodium.so pixmap tinysparql girepository mypaint $lib_audio_keywords $lib_browser_launcher"
 
 [ -n "$lib_browser_launcher" ] && DEPENDENCES="$DEPENDENCES xapp hicolor-icon-theme"
 
