e4f0ccf684376194a5af129a2e14a8bc7e6c5236
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-11-20 16:56:13 +0100

    Update gimp-git-junest.sh

--- gimp-git-junest.sh
+++ gimp-git-junest.sh
@@ -140,5 +140,4 @@ _appimagetool() {
 	fi
 }
 
-ARCH=x86_64 _appimagetool -u "$UPINFO" \
-	AppDir "$APPNAME"_GIT_"$VERSION"-archimage5.0-x86_64.AppImage
+ARCH=x86_64 _appimagetool -u "$UPINFO" AppDir "$APPNAME"_GIT_"$VERSION"-"$ARCHIMAGE_VERSION"-x86_64.AppImage
